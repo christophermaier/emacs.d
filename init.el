0ced2c9639f6d4e0542e2eecfde1ddd1bbc35de3
Christopher Maier <christopher.maier@gmail.com>
2010-08-10 07:02:20 -0400

    Changed the default font from Monaco to Andale Mono on Mac OS X (I like the lower-case "a" better... doesn't look like an "o")

--- init.el
+++ init.el
@@ -33,3 +33,30 @@
 (require 'markdown-config)
 (require 'whitespace-config)
 (require 'magit-config)
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil
+		:stipple nil
+		:background "#3f3f3f"
+		:foreground "#dcdccc"
+		:inverse-video nil
+		:box nil
+		:strike-through nil
+		:overline nil
+		:underline nil
+		:slant normal
+		:weight normal
+		:height 140
+		:width normal
+		:foundry "apple"
+		:family "Andale Mono")))))

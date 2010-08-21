340a314ca8a9417c5fe948e948ee1d7ed7b70ad1
Christopher Maier <christopher.maier@gmail.com>
2010-08-21 08:02:21 -0400

    Getting fancy in Clojure mode; saving now fixes whitespace errors, saves, and compiles.  F8 also runs 'slime-connect'.

--- clojure-config.el
+++ clojure-config.el
@@ -2,16 +2,16 @@
 
 ;; Let's turn on Paredit by default, shall we?
 (add-hook 'clojure-mode-hook
-	  (lambda () 
-	    (paredit-mode +1)))
+      (lambda ()
+        (paredit-mode +1)))
 
 ;; Paredit would be sweet to have in the REPL, too
 ;; Strictly speaking, this could probably go somewhere other than a Clojure
 ;; configuration file, but I currently don't do any REPL work in anything
 ;; but Clojure, so here it stays
 (add-hook 'slime-repl-mode-hook
-	  (lambda ()
-	    (paredit-mode +1)))
+      (lambda ()
+        (paredit-mode +1)))
 
 ;;; Fix indentation for Lazytest functions (as per http://github.com/stuartsierra/lazytest)
 (eval-after-load 'clojure-mode
@@ -22,5 +22,15 @@
      (defmulti 'defun)
      (defmethod 'defun)))
 
+(add-hook 'clojure-mode-hook
+  '(lambda ()
+     ;; Fix whitespace errors, save, and compile instead of just saving
+     (define-key clojure-mode-map [remap save-buffer] (lambda ()
+                                                        (interactive)
+                                                        (whitespace-cleanup)
+                                                        (save-buffer)
+                                                        (slime-compile-and-load-file)))
+     ;; Handy key for hooking up to swank
+     (define-key clojure-mode-map [f8] 'slime-connect)))
 
 (provide 'clojure-config)

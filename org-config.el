4ff9138bafc1efd7dbcebb8b641e6b311d66b319
Christopher Maier <christopher.maier@gmail.com>
2010-11-13 13:11:47 -0500

    Set some refile targets

--- org-config.el
+++ org-config.el
@@ -112,5 +112,6 @@
           (org-agenda-entry-types '(:scheduled))
           (org-agenda-sorting-strategy '(time-up habit-up category-up tag-down))))))
 
+(setq org-refile-targets '((org-agenda-files . (:level . 1))))
 
 (provide 'org-config)

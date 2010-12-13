67b36cfb33ccd4bcd2c94711fae558a08942fa42
Christopher Maier <christopher.maier@gmail.com>
2010-12-13 06:40:40 -0500

    Add custom snippets directory

--- yasnippet-config.el
+++ yasnippet-config.el
@@ -1,6 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d/ext/yasnippet-0.6.1c")
 (require 'yasnippet) ;; not yasnippet-bundle
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/ext/yasnippet-0.6.1c/snippets")
+
+(setq yas/root-directory '("~/.emacs.d/snippets"
+                           "~/.emacs.d/ext/yasnippet-0.6.1c/snippets"))
+(mapc 'yas/load-directory yas/root-directory)
 
 (provide 'yasnippet-config)

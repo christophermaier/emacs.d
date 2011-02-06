a14cec1f3aa31f188c0c6ad9160f436b986f90e0
Christopher Maier <christopher.maier@gmail.com>
2011-02-06 07:30:23 -0500

    Add MozRepl, use it with js2-mode, and finally disable autopair with js2

--- javascript-config.el
+++ javascript-config.el
@@ -1,11 +1,19 @@
-(autoload 'espresso-mode "espresso" "Start espresso-mode" t)
-(add-to-list 'auto-mode-alist '("\\.js$" . espresso-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . espresso-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
 ;; (defun javascript-mode-config ()
 ;;   (highlight-parentheses-mode t))
 
 ;; (add-hook 'javascript-mode-hook 'javascript-mode-config)
 
+(autoload 'moz-minor-mode "moz" "Mozilla Minor and Inferior Mozilla Modes" t)
+
+(autoload 'js2-mode "js2-mode" "Start JS2 Mode" t)
+
+(defun js2-custom-setup ()
+  (moz-minor-mode 1)
+  (setq autopair-dont-activate t))
+
+(add-hook 'js2-mode-hook 'js2-custom-setup)
 
 (provide 'javascript-config)

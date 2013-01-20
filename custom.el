41df2d70622738f22cd70e95ce06b724ccd58b3a
Christopher Maier <cm@opscode.com>
2013-01-20 08:31:16 -0500

    Fix colors

--- custom.el
+++ custom.el
@@ -4,11 +4,3 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(ido-everywhere t))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#3f3f3f" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :family "Anonymous Pro"))))
- '(hl-line ((t (:background "#5f5f5f"))))
- '(org-hide ((t (:foreground "#3f3f3f")))))

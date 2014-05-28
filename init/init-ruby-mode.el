06d78968dbcbfaac5ebe8eccba1c0f4728f5fbd2
Christopher Maier <christopher.maier@gmail.com>
2014-05-28 00:10:41 -0400

    Add ruby-mode, ruby-tools back

--- init/init-ruby-mode.el
+++ init/init-ruby-mode.el
@@ -1,3 +1,4 @@
+(add-to-list 'auto-mode-alist '("Berksfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))

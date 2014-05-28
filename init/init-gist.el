bcd503acfff1d8b026d78df22b555219265fbd5c
Christopher Maier <christopher.maier@gmail.com>
2014-05-28 00:01:29 -0400

    Modernize git install, remove magithub

---
+++ init/init-gist.el
@@ -0,0 +1,7 @@
+;; https://github.com/defunkt/gist.el
+
+;; View a gist once it has been created
+(setq gist-view-gist t)
+
+;; Sadly suffering from https://github.com/defunkt/gist.el/issues/57, still :(
+;; Also see https://github.com/defunkt/gist.el/issues/51#issuecomment-34169501

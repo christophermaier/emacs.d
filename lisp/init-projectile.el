35d97f2f39edc464c471383b049db49cb3812ee5
Christopher Maier <cmaier@chef.io>
2019-06-28 12:07:05 -0400

    Update projectile configuration

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -2,7 +2,9 @@
 (require-package 'helm-projectile)
 
 (with-eval-after-load 'projectile
-  (projectile-global-mode)
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (setq projectile-use-git-grep t))
 
 ;; Some key commands:

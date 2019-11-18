78b75bdf954bb0f97fd52ba76c576c564577ab2e
Christopher Maier <cmaier@chef.io>
2019-11-18 09:02:00 -0500

    Update rustfmt binary
    Signed-off-by: Christopher Maier <cmaier@chef.io>

--- init.el
+++ init.el
@@ -173,6 +173,7 @@
   :mode "\\.rs\\'"
   :custom
   (rust-format-on-save t)
+  (rust-rustfmt-bin "/home/maier/.rustup/toolchains/nightly-2019-10-15-x86_64-unknown-linux-gnu/bin/rustfmt")
   :bind
   (:map rust-mode-map
         ([tab] . company-indent-or-complete-common)))

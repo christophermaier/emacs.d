fde0565a9b2c1fc0dc63547723285d64ebf61e32
Christopher Maier <christopher.maier@gmail.com>
2019-11-17 21:14:19 -0500

    Configure highlight-symbol via use-package

--- lisp/init-prog-mode.el
+++ lisp/init-prog-mode.el
@@ -25,15 +25,12 @@
 
 ;; highlight-symbol
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(require-package 'highlight-symbol)
-(add-hook 'prog-mode-hook 'highlight-symbol-nav-mode)
-(with-eval-after-load 'highlight-symbol
-  ;; M-n and M-p already do next / prev symbol navigation
-  (define-key highlight-symbol-nav-mode-map (kbd "M-N") 'highlight-symbol-next-in-defun)
-  (define-key highlight-symbol-nav-mode-map (kbd "M-P") 'highlight-symbol-prev-in-defun)
-
-  (define-key highlight-symbol-nav-mode-map (kbd "<f9>") 'highlight-symbol)
-  (define-key highlight-symbol-nav-mode-map (kbd "C-<f9>") 'highlight-symbol-query-replace))
-;; TODO: try and cook up a highlight-symbol-query-replace-in-defun
+(use-package highlight-symbol
+  :hook (prog-mode . highlight-symbol-nav-mode)
+  :bind (:map highlight-symbol-nav-mode-map
+              ("M-N" . highlight-symbol-next-in-defun)
+              ("M-P" . highlight-symbol-prev-in-defun)
+              ("<f9>" . highlight-symbol)
+              ("C-<f9>" . highlight-symbol-query-replace)))
 
 (provide 'init-prog-mode)
